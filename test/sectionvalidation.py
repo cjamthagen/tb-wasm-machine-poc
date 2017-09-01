85b409f03ba621efd78e5c888e30d40719d32a72
Christopher Jämthagen <cjamthagen@gmx.com>
2017-09-01 15:33:25 +0200

    Add skeleton code for test coverage of section validation fails

--- test/sectionvalidation.py
+++ test/sectionvalidation.py
@@ -38,8 +38,35 @@ def section_validation_fail(directory):
             if "type" in directory:
                 if not modulevalidation.TypeSection():
                     sys.exit(1)
+            if "import" in directory:
+                if not modulevalidation.ImportSection():
+                    sys.exit(1)
+            if "function" in directory:
+                if not modulevalidation.FunctionSection():
+                    sys.exit(1)
+            if "table" in directory:
+                if not modulevalidation.TableSection():
+                    sys.exit(1)
+            if "memory" in directory:
+                if not modulevalidation.MemorySection():
+                    sys.exit(1)
             if "global" in directory:
                 if not modulevalidation.GlobalSection():
+                    sys.exit(1)
+            if "export" in directory:
+                if not modulevalidation.ExportSection():
+                    sys.exit(1)
+            if "start" in directory:
+                if not modulevalidation.StartSection():
+                    sys.exit(1)
+            if "element" in directory:
+                if not modulevalidation.ElementSection():
+                    sys.exit(1)
+            if "code" in directory:
+                if not modulevalidation.CodeSection():
+                    sys.exit(1)
+            if "data" in directory:
+                if not modulevalidation.DataSection():
                     sys.exit(1)
             sys.exit()
         # the parent process
---
+++ test/testsuite_fail/README.md
@@ -0,0 +1,1 @@
+This directory will contain invalid wasm files aimed at testing section validation and that they fail in a correct manner.
